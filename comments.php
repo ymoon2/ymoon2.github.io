04154c1d5a2fd1892ee72140f4b0fb5f5f229c06
ymoon2 <ymoon2@vt.edu>
2019-12-06 19:43:31 -0500

    editting submit

--- comments.php
+++ comments.php
@@ -1,2 +1,3 @@
-// Check if the form is submitted if ( isset( $_POST['submit'] ) ) { // retrieve the form data by using the element's name attributes value as key $firstname = $_POST['firstname']; $lastname = $_POST['lastname']; // display the results
-echo '<h3>Form POST Method</h3>'; echo 'Name' . $name . 'Email' . $email . 'Message' . $message; exit; } 
+<?php
+ $_POST['variable_name'];
+?>
